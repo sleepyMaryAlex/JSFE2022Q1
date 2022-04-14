213d27d36d3808a6b60bc95f5b16c7e33684e707
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-14 10:15:23 +0300

    feat: add addEventListener for nav links

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,9 +1,16 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-link");
 
 burger.addEventListener("click", showMenu);
+navLinks.forEach(link => link.addEventListener("click",  () => {
+    if (link.classList.contains("menu-link")) {
+        showMenu();
+    }
+}));
 
 function showMenu() {
     burger.classList.toggle("rotated");
     menu.classList.toggle("menu-visible");
+    navLinks.forEach(link => link.classList.toggle("menu-link"));
 }
