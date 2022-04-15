ed4f799d7ebfa3274a74bdfe48518c1894b3fb8b
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-15 16:06:56 +0300

    feat: add choosePets function

--- shelter/pages/main/script/script.js
+++ shelter/pages/main/script/script.js
@@ -1,1 +1,23 @@
 import pets from "./pets.js";
+
+function choosePets(pets) {
+    let count;
+    let indexes = [];
+    if (body.clientWidth < 768) {
+        count = 1;
+    } else if (body.clientWidth < 1280) {
+        count = 2;
+    } else {
+        count = 3;
+    }
+
+    while (indexes.length < count) {
+        let n = Math.floor(Math.random() * 8); 
+        if (!indexes.includes(n)) {
+            indexes.push(n);
+        }
+    }
+    return indexes;
+}
+
+choosePets(pets);
