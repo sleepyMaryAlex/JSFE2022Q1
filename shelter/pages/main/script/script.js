8ab68ca8f60fcf85ddf811f3501bf66531a8b6c8
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-15 16:46:08 +0300

    feat: add showPets function

--- shelter/pages/main/script/script.js
+++ shelter/pages/main/script/script.js
@@ -1,6 +1,6 @@
 import pets from "./pets.js";
 
-function choosePets(pets) {
+function choosePets() {
     let count;
     let indexes = [];
     if (body.clientWidth < 768) {
@@ -20,4 +20,32 @@ function choosePets(pets) {
     return indexes;
 }
 
-choosePets(pets);
+const indexes = choosePets();
+
+const img1 = document.querySelector(".card-1 img");
+const img2 = document.querySelector(".card-2 img");
+const img3 = document.querySelector(".card-3 img");
+const name1 = document.querySelector(".card-1 h4");
+const name2 = document.querySelector(".card-2 h4");
+const name3 = document.querySelector(".card-3 h4");
+
+function showPets(pets, indexes) {
+    if (indexes.length === 1) {
+        img1.setAttribute("src", pets[indexes[0]].img);
+        name1.textContent = pets[indexes[0]].name;
+    } else if (indexes.length === 2) {
+        img1.setAttribute("src", pets[indexes[0]].img);
+        img2.setAttribute("src", pets[indexes[1]].img);
+        name1.textContent = pets[indexes[0]].name;
+        name2.textContent = pets[indexes[1]].name;
+    } else {
+        img1.setAttribute("src", pets[indexes[0]].img);
+        img2.setAttribute("src", pets[indexes[1]].img);
+        img3.setAttribute("src", pets[indexes[2]].img);
+        name1.textContent = pets[indexes[0]].name;
+        name2.textContent = pets[indexes[1]].name;
+        name3.textContent = pets[indexes[2]].name;
+    }
+}
+
+showPets(pets, indexes);
