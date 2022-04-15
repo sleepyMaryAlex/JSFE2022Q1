ae38c62ae5c8c24528031d4ae239db28d87a404a
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-15 14:55:14 +0300

    feat: add menu.js

--- shelter/pages/main/script/menu.js
+++ shelter/pages/main/script/menu.js
@@ -1,6 +1,9 @@
+// ====== Menu ======
+
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".nav");
 const body = document.querySelector("body");
+const header = document.querySelector(".header");
 const logoLink = document.querySelector(".logo-link");
 const navLinks = document.querySelectorAll(".nav-link");
 
@@ -19,12 +22,12 @@ body.addEventListener("click", (e) => {
     }
 }, true);
 
-
 function showMenu() {
     burger.classList.toggle("rotated");
     menu.classList.toggle("menu-visible");
     navLinks.forEach(link => link.classList.toggle("menu-link"));
     logoLink.classList.toggle("logo-invisible");
     body.classList.toggle("body-overflow");
+    header.classList.toggle("header-position");
     body.classList.toggle("body-overlay");
 }
