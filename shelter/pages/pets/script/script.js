f137f796a083c5ed2ae5849a0d2cabb8a2b79908
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-24 08:52:33 +0300

    feat: add button interactivity on hover overlay

--- shelter/pages/pets/script/script.js
+++ shelter/pages/pets/script/script.js
@@ -1,1 +1,17 @@
-import pets from "../../main/script/pets.js"; 
+import pets from "../../main/script/pets.js";
+
+let array = [];
+
+function createArray() {
+    for (let i = 0; i < 6; i++) {
+        let subArray = [];
+        while (subArray.length < 8) {
+          let n = Math.floor(Math.random() * 8);
+          if (!subArray.includes(n)) {
+            subArray.push(n);
+            array.push(n);
+          }
+        }
+    }
+}
+createArray();
