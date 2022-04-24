4dbc2047e80328f0a22bb5471714fc9ddf9bf468
Maria Guchkova <mariaguchkova1@gmail.com>
2022-04-24 12:00:39 +0300

    refactor: edit showPets function

--- shelter/pages/main/script/slider.js
+++ shelter/pages/main/script/slider.js
@@ -2,12 +2,7 @@ import pets from "./pets.js";
 
 // ======= Slider =======
 
-const img1 = document.querySelector(".card-1 img");
-const img2 = document.querySelector(".card-2 img");
-const img3 = document.querySelector(".card-3 img");
-const name1 = document.querySelector(".card-1 h4");
-const name2 = document.querySelector(".card-2 h4");
-const name3 = document.querySelector(".card-3 h4");
+const cards = document.querySelectorAll(".card");
 const arrowButtons = document.querySelectorAll(".arrow");
 
 arrowButtons.forEach(btn => btn.addEventListener("click", choosePets));
@@ -31,12 +26,10 @@ function choosePets() {
 }
 
 function showPets(pets, indexes) {
-        img1.setAttribute("src", pets[indexes[0]].img);
-        img2.setAttribute("src", pets[indexes[1]].img);
-        img3.setAttribute("src", pets[indexes[2]].img);
-        name1.textContent = pets[indexes[0]].name;
-        name2.textContent = pets[indexes[1]].name;
-        name3.textContent = pets[indexes[2]].name;
+    for (let i = 0; i < indexes.length; i++) {
+        cards[i].children[0].setAttribute("src", pets[indexes[i]].img);
+        cards[i].children[1].textContent = pets[indexes[i]].name;
+    }
 }
 
 function saveToLocalStorage(indexes) {
