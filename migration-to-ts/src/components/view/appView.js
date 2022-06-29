7fffbd7002f9c933dd0682507fabdf642f79f750
Maria Guchkova <mariaguchkova1@gmail.com>
2022-06-29 15:16:25 +0300

    feat: migrate appView.js to appView.ts

--- migration-to-ts/src/components/view/appView.js
+++ migration-to-ts/src/components/view/appView.js
@@ -1,21 +1,23 @@
-import News from './news/news';
-import Sources from './sources/sources';
-
-export class AppView {
-    constructor() {
-        this.news = new News();
-        this.sources = new Sources();
+"use strict";
+exports.__esModule = true;
+exports.AppView = void 0;
+var news_1 = require("./news/news");
+var sources_1 = require("./sources/sources");
+var AppView = /** @class */ (function () {
+    function AppView() {
+        this.news = new news_1["default"]();
+        this.sources = new sources_1["default"]();
     }
-
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
+    AppView.prototype.drawNews = function (data) {
+        var values = (data === null || data === void 0 ? void 0 : data.articles) ? data === null || data === void 0 ? void 0 : data.articles : [];
         this.news.draw(values);
-    }
-
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
+    };
+    AppView.prototype.drawSources = function (data) {
+        console.log(data);
+        var values = (data === null || data === void 0 ? void 0 : data.sources) ? data === null || data === void 0 ? void 0 : data.sources : [];
         this.sources.draw(values);
-    }
-}
-
-export default AppView;
+    };
+    return AppView;
+}());
+exports.AppView = AppView;
+exports["default"] = AppView;
---
+++ migration-to-ts/src/components/view/appView.ts
@@ -0,0 +1,52 @@
+import News from './news/news';
+import Sources from './sources/sources';
+
+interface DataArticles {
+    articles: {
+        author: string;
+        content: string;
+        description: string;
+        publishedAt: string;
+        source: {
+            id: string;
+            name: string;
+        };
+        title: string;
+        url: string;
+        urlToImage: string;
+    }[];
+}
+
+interface DataSources {
+    sources: {
+        category: string;
+        country: string;
+        description: string;
+        id: string;
+        language: string;
+        name: string;
+        url: string;
+    }[];
+}
+
+export class AppView {
+    news: News;
+    sources: Sources;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: DataArticles) {
+        const values = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: DataSources) {
+        console.log(data);
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
