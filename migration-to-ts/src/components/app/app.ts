c1c1a2dd585215ced3f57dab2e6f1018867d2d67
Maria Guchkova <mariaguchkova1@gmail.com>
2022-07-04 12:26:14 +0300

    feat: implement search for news by phrase

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -15,6 +15,11 @@ class App {
             this.controller.getNews<IDataArticles>(e, (data) => this.view.drawNews(data as IDataArticles))
         );
         this.controller.getSources<IDataSources>((data) => this.view.drawSources(data as IDataSources));
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                this.controller.getNewsByPhrase<IDataArticles>((data) => this.view.drawNews(data as IDataArticles));
+            }
+        });
     }
 }
 
--- migration-to-ts/src/components/controller/controller.ts
+++ migration-to-ts/src/components/controller/controller.ts
@@ -13,6 +13,9 @@ class AppController extends AppLoader {
     }
 
     public getNews<T>(e: Event, callback: CallbackGeneric<T>) {
+        const input: HTMLElement = document.querySelector('.input') as HTMLElement;
+        (input as HTMLInputElement).value = '';
+
         let target: targetType = e.target;
         const newsContainer: targetType = e.currentTarget;
 
@@ -34,6 +37,23 @@ class AppController extends AppLoader {
                 return;
             }
             target = (target as HTMLElement).parentNode;
+        }
+    }
+
+    public getNewsByPhrase<T>(callback: CallbackGeneric<T>) {
+        const input: HTMLElement = document.querySelector('.input') as HTMLElement;
+        const inputValue: string = (input as HTMLInputElement).value;
+
+        if (inputValue.trim() !== '') {
+            super.getResp<T>(
+                {
+                    endpoint: 'everything',
+                    options: {
+                        q: inputValue,
+                    },
+                },
+                callback
+            );
         }
     }
 }
